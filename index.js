0cbbcbedf127189b176234ce6a7dc4b1f906afd6
Cameron Paczek <cpazek@gmail.com>
2022-09-07 16:37:58 -0700

    another simple change

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 module.exports = function verifyEmail(email){
-    console.log("remove this log to demonstrate git")
+    console.log("remove this log to demonstrate git different change")
     let re = /\S+@\S+\.\S+/;
     return re.test(email);
 }
